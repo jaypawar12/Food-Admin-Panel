31347d3358971b2c8ed8f491c296674591619150
jaypawar12 <pawarjay684@gmail.com>
2025-06-21 17:17:43 +0530

    Admin Panel

--- config/adminDB.js
+++ config/adminDB.js
@@ -2,11 +2,10 @@ require('dotenv').config(); // only needed locally
 
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://jaystack12:jay123@cluster0.zoep9wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const URI = "mongodb+srv://jaystack12:jay123@cluster0.zoep9wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+const URI = "mongodb://localhost:27017/Food-Admin-Panel";
+
+mongoose.connect(URI)
     .then(() => console.log("DB Connected...."))
     .catch((err) => console.log("DB Not Connected....", err));
